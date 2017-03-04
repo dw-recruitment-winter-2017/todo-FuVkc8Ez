7031cec7cfaf868b89e5e0802d828f14c7a6592f
Anonymous Applicant <anonymous@example.com>
2017-03-04 17:53:09 -0500

    basic front end functionality

--- src/cljs/todo_test/core.cljs
+++ src/cljs/todo_test/core.cljs
@@ -4,12 +4,66 @@
               [secretary.core :as secretary :include-macros true]
               [accountant.core :as accountant]))
 
+(defonce todos (reagent/atom (sorted-map)))
+(defonce counter (reagent/atom 0))
+
+(defn add-todo [text]
+  (let [id (swap! counter inc)]
+    (swap! todos assoc id {:id id :title text :done false})))
+
+(defn save [id title] (swap! todos assoc-in [id :title] title))
+
+(defn todo-input [{:keys [title on-save on-stop]}]
+  (let [val (reagent/atom title)
+        stop #(do (reset! val "")
+                  (if on-stop (on-stop)))
+        save #(let [v (-> @val str clojure.string/trim)]
+                (if-not (empty? v) (on-save v))
+                (stop))]
+    (fn [{:keys [id class placeholder]}]
+      [:input {:type "text" :value @val
+               :id id :class class :placeholder placeholder
+               :on-blur save
+               :on-change #(reset! val (-> % .-target .-value))
+               :on-key-down #(case (.-which %)
+                               13 (save)
+                               27 (stop)
+                               nil)}])))
+
+(defn todo-item []
+ (let [editing (reagent/atom false)]
+   (fn [{:keys [id done title]}]
+     [:li
+      [:div.view
+       [:input.toggle {:type "checkbox"}]
+       [:label {:on-double-click #(reset! editing true)} title]]])))
+
+(defn todo-app [props]
+    (fn []
+      (let [items (vals @todos)]
+        [:div
+         [:section#todoapp
+          [:header#header
+           [:h1 "todos"]
+           [todo-input {:id "new-todo"
+                        :placeholder "What needs to be done?"
+                        :on-save add-todo}]]
+          (when (-> items count pos?)
+            [:div
+             [:section#main
+              [:input#toggle-all {:type "checkbox"}]
+
+              [:ul#todo-list
+               (for [todo items]
+                 ^{:key (:id todo)} [todo-item todo])]]])]])))
+
 ;; -------------------------
 ;; Views
 
 (defn home-page []
   [:div [:h2 "Welcome to todo-test"]
-   [:div [:a {:href "/about"} "go to about page"]]])
+   [:div [:a {:href "/about"} "go to about page"]]]
+  [todo-app])
 
 (defn about-page []
   [:div [:h2 "About todo-test"]
