c9655c59b0bf82b524e54f97a07bb85b462262b8
Anonymous Applicant <anonymous@example.com>
2017-03-18 09:13:58 -0400

    Added 1st round of documentation
    also fixed a couple syntax errors so the app launches successfully.

--- src/clj/todo_test/handler.clj
+++ src/clj/todo_test/handler.clj
@@ -1,11 +1,13 @@
 (ns todo-test.handler
-  (:require [compojure.core :refer [GET defroutes]]
+  (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [not-found resources]]
             [hiccup.page :refer [include-js include-css html5]]
             [todo-test.middleware :refer [wrap-middleware]]
             [config.core :refer [env]]))
 
+;; This is called into loading-page
 (def mount-target
+  "Placeholder and indicator rendered if figwheel is not running."
   [:div#app
       [:h3 "ClojureScript has not been compiled!"]
       [:p "please run "
@@ -13,44 +15,63 @@
        " in order to start the compiler"]])
 
 (defn head []
+  "This is a function that renders the hiccup template for the site"
   [:head
    [:meta {:charset "utf-8"}]
    [:meta {:name "viewport"
            :content "width=device-width, initial-scale=1"}]
    (include-css (if (env :dev) "/css/site.css" "/css/site.min.css"))])
 
-;; The single source of truth, the database
-;; {0 {:id 0 :text "a b c" :status "open"}}
-(def todolist-atom (atom {}))
-
-;; Okay, so we need a secondary truth for the id on each item
-(def counter-atom (atom 0))
-
 (defn loading-page []
+  "This is a function for the page rendering on load"
   (html5
     (head)
     [:body {:class "body-container"}
      mount-target
      (include-js "/js/app.js")]))
 
-;; returns the dereferenced todolist-atom content
+;; The single source of truth, the "database"
+;; expected structure: {0 {:text "a b c" :complete true}}
+(def todolist-atom (atom {}))
+
+;; Okay, so we need a secondary truth for the id on each item
+(def counter-atom (atom 0))
+
+;; This function returns the dereferenced todolist-atom content
 (defn list-get [] @todolist-atom)
 
+;; This function
+;; - accepts the params from the post request
+;; - increments the counter-atom and binds it to id
+;; - binds the :list from params to new-list-str
+;; - converts new-list-str to edn syntax and binds it to new-list
+;; - associates the incremented id into the new todo item
+;; - associates the new todo item including the incremented id to the content of the todolist-atom
+;; - swaps it into the todolist-atom
 (defn list-add
+  "This function accepts the params from the POST request and swaps it into the todolist-atom"
   [params]
   (let [id (swap! counter-atom inc)
         new-list-str (:list params)
         new-list (clojure.edn/read-string new-list-str)]
     (swap! todolist-atom
       (fn [todolist]
-        (assoc todolist id (assoc new-list :id id))))))
+        (assoc todolist id (assoc new-list))))))
 
+;; Backend Routes
 (defroutes routes
+  ;; gets the html template for the home page
   (GET "/" [] (loading-page))
+
+  ;; gets the html template for the about page
   (GET "/about" [] (loading-page))
+
+  ;; gets the result of list-get
   (GET "/list" [] (list-get))
+
+  ;; gives the request to the list-add function
   (POST "/list" request
-    (list-add (:params request))
+    (list-add (:params request)))
 
   (resources "/")
   (not-found "Not Found"))
--- src/cljs/todo_test/core.cljs
+++ src/cljs/todo_test/core.cljs
@@ -5,35 +5,59 @@
               [accountant.core :as accountant]
               [ajax.core :refer [POST]]))
 
+;; This is our front end source of truth using the reagent/react version of an atom
 (defonce state-atom
  (reagent/atom
    {:todos {}
     :text nil}))
 
-(defn add-todo [text]
+;; This function adds a new to do item by POSTing to the backend and then updating the list with the response
+; (defn add-todo [text]
   ;; POST to backend
   ;; improvement: add an error handler
-  (POST "/list"
-        {:params {0
-          {:id 0
-          :text "a b c"
-          :open true}}
-          :handler (fn [data]
-            new-state (clojure.edn/read-string data)))})
+  ; (POST "/list"
+  ;       {:params {0
+  ;                 {:text "a b c"
+  ;                  :open true}}
+  ;         :handler (fn [data]
+  ;                   new-state (clojure.edn/read-string data))}))
   ;; ^ Take backend's response
   ;; Parse response with edn and put it in state-atom
   ;; swap! todos with new db value
-  )
 
+
+;; This function updates the status
+;; improvement: expand this function or write a new one that will allow the
+(defn update-todo [id])
+  ;; POST to backend
+  ;; Take backend's response
+  ;; swap! todos with new db value
+
+
+;; This function removes a todo from the list of todos
+(defn remove-todo [id])
+  ;; DELETE to backend
+  ;; Take backend's response
+  ;; Parse response
+  ;; reset! todos with new db value
+
+
+;; This function handles the value in the input, initially set to nil
+;; The button triggers the click to fire the add-todo function
 (defn todo-input [text]
-  [:input {:type "text" :value text
-           :id "new-todo" :placeholder "What needs to be done?"
-           :on-change
-           #(swap! state-atom
-             (fn [state]
-               (assoc state :text (-> % .-target .-value))))]
-           ))
+  [:input {:type "text" :value text :id "new-todo" :placeholder "What needs to be done?"
+           :on-change #(swap! state-atom
+                          (fn [state]
+                           ;; when you get a property from a javascript object in clojure it needs a dash
+                           ;; we get targeted value from the state object that's been passed in
+                           (assoc state :text (-> % .-target .-value))))}])
+  ; [:button {:on-click}])
+    ;; Takes the text, turn it into a todo data structure
+    ;; resets text to nil
 
+
+;; This function handles the todo items themselves.
+;; On the checkbox click, update-todo is called
 (defn todo-item [{:keys [title completed]}]
   [:li
     [:div.view
@@ -43,6 +67,13 @@
 ;; -------------------------
 ;; Views
 
+;; This function renders the view for the home page
+;; The let portion does the following:
+;; - binds the dereferenced state-atom to state
+;; - binds the values of the todo list to items
+;; - calls todo-input and passes in the state
+;; - checks that there are todos in items
+;; - as long as there are todo items, loops through each of them and passes that todo to todo-item to be rendered
 (defn home-page []
   [:main
    [:div#heading [:h2 "Welcome to your To Do List"]
@@ -57,16 +88,19 @@
             (for [todo items]
               (todo-item todo))]])])])
 
+;; This function renders the view for the about page
 (defn about-page []
   [:main
    [:div#heading [:h2 "About your To Do List"] [:a {:href "/"} "Back to the list"]]
    [:p "This is a simple To Do list app built with Clojure using reagent and compojure."]])
 
+;; This function gets the current page and inserts it into a div
 (defn current-page []
   [:div [(session/get :current-page)]])
 
 ;; -------------------------
 ;; Routes
+;; Both routes do the same thing, take the uri and apply the appropriate page
 
 (secretary/defroute "/" []
   (session/put! :current-page #'home-page))
@@ -78,12 +112,15 @@
 ;; -------------------------
 ;; Initialize app
 
+;; This function will:
+;; - GET /list the current state of the todolist-atom from the backend
+;; - parse the result
+;; - swap! the state-atom with that parsed result
+;; - render the current page
 (defn mount-root []
-  ;; GET /list
-  ;; Parse the result
-  ;; swap! your atom with that parsed result
   (reagent/render [current-page] (.getElementById js/document "app")))
 
+;; This function initiates the whole app
 (defn init! []
   (accountant/configure-navigation!
     {:nav-handler
